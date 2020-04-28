04eebc38ef9d33c877fd182bd0f657a3542f0fef
Chris-Pyy <55000165+Chris-Pyy@users.noreply.github.com>
2020-04-28 17:44:25 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -23,12 +23,32 @@ app.get('/', function(req, res) {
 let token = "EAADTXVXZCI8gBAOgwCXniZCsoLEwha26qKQPsojuVxPZApCEQgQYXsVdgZBzdDXWZCuJzNyHZBMcs08FRStijjsjXWOhM5RolKrT0Rbfuq4CX3gZBklcgoa5oYnlITH8Th7xsJ6BKNr99yGeZA5644XXIFQqAhzNEYiII26EfkZABnwZDZD"
 
 //facebook
-app.get('/webhook/', function(req, res) {
-	if (req.query['hub.verify_token'] == "carlos") {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send("incorrect token")
-})
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "carlos"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
 
 app.post('/webhook/', function(req, res) {
 	let messaging_events = req.body.entry[0].messaging
