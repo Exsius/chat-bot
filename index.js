e49dfc2981c98705e9b01ddf67a1da94fb3b6fd8
Chris-Pyy <55000165+Chris-Pyy@users.noreply.github.com>
2020-04-28 17:24:14 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function sendText(sender, text) {
 	let messageData = {text: text}
 	request ({
 		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs : {access_token, token},
+		qs : {access_token : token},
 		method: "POST",
 		json: {
 			recipient: {id: sender},
