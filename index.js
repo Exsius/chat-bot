dbe03538ae98ed70eb1ad074c64e24604cc7792e
Chris-Pyy <55000165+Chris-Pyy@users.noreply.github.com>
2020-04-28 17:46:22 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
 
-const app = express()
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-//set port or else set to 5000
-app.set('port', (process.env.PORT || 5000))
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
 
-//allows program to process the data
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
 
-//routes
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
-//default
-app.get('/', function(req, res) {
-	res.send("Hello World")
-})
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
 
-let token = "EAADTXVXZCI8gBAOgwCXniZCsoLEwha26qKQPsojuVxPZApCEQgQYXsVdgZBzdDXWZCuJzNyHZBMcs08FRStijjsjXWOhM5RolKrT0Rbfuq4CX3gZBklcgoa5oYnlITH8Th7xsJ6BKNr99yGeZA5644XXIFQqAhzNEYiII26EfkZABnwZDZD"
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 
-//facebook
+});
+
+// Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "carlos"
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
     
   // Parse the query params
   let mode = req.query['hub.mode'];
@@ -49,41 +62,3 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
-
-app.post('/webhook/', function(req, res) {
-	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
-		let event = messaging_events[i]
-		let sender = event.sender.id
-		if (event.mssage && event.message.text) {
-			let text = event.message.text
-			sendText(sender, "text echo" + text.substring(0, 100))
-		}
-	}
-	res.sendStatus(200)
-})
-
-
-function sendText(sender, text) {
-	let messageData = {text: text}
-	request ({
-		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs : {access_token : token},
-		method: "POST",
-		json: {
-			recipient: {id: sender},
-			message: messageData
-            }
-		}, function (error, response, body) {
-			if (error) {
-				console.log("error")
-			} else if (response.body.error){
-				console.log("body error")
-			}
-	})
-}
-
-
-app.listen(app.get('port'), function() {
-	console.log("running: port")
-})
