bece038a99cf6ffb8fb74f0b055c04479f5459d7
Chris-Pyy <55000165+Chris-Pyy@users.noreply.github.com>
2020-04-28 18:21:04 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function sendText(sender, text) {
 		method: "POST",
 		json: {
 			recipient: {id: sender},
-			message: messageData
+			message: messageData,
             }
 		}, function (error, response, body) {
 			if (error) {
